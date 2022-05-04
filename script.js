9d6e949a8193269176333fd7a7f3d5c51c6280da
Christopher <tikras8@gmail.com>
2022-05-04 22:18:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var person = ["Jon Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"]
+console.log (person[0], person[5])
+var numero = prompt("Įveskite skaičių nuo 1 iki 6")
+if (numero == 1) console.log(person[0])
+if (numero == 2) console.log(person[1])
+if (numero == 3) console.log(person[2])
+if (numero == 4) console.log(person[3])
+if (numero == 5) console.log(person[4])
+if (numero == 6) console.log(person[5])
